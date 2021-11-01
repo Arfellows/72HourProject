cef2cae7235610d564e6cd1e31a4d640a83b90f6
Ashley Fellows <ashley.r.fellows@gmail.com>
2021-11-01 19:43:18 -0400

    added post model and controller

---
+++ 72HourProject/Models/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _72HourProject.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public Guid AuthorId { get; set; }
+        public string Comments { get; set; }
+
+        //public virtual List<Comment> {get; set;}
+
+    }
+}
---
+++ 72HourProject/Models/PostDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _72HourProject.Models
+{
+    public class PostDbContext : DbContext
+    {
+        public PostDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        //public DbSet<Comment> Comments { get; set; }
+    }
+}
