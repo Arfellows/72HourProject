9c2f58bd479c54748f63e6c988f6e1658860b0bd
Johnson Lee <johnson.hwang1008@gmail.com>
2021-11-02 17:49:35 -0400

    Initial commit

---
+++ 72HourProject.Models/ReplyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72HourProject.Models
+{
+    public class ReplyDbContext : DbContext
+    {
+        public ReplyDbContext() : base("ReplyConnection")
+        {
+
+        }
+        public DbSet<Reply> Replies { get; set; }
+    }
+}
